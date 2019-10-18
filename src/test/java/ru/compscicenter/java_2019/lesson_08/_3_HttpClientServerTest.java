df11e05a5b3c81a5f8987c57cad9811a0df98608
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2019-10-18 20:18:48 +0300

    добавил тесты для 8-го урока

---
+++ src/test/java/ru/compscicenter/java_2019/lesson_08/_3_HttpClientServerTest.java
@@ -0,0 +1,43 @@
+package ru.compscicenter.java_2019.lesson_08;
+
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static ru.compscicenter.java_2019.Util.__;
+
+@FixMethodOrder(MethodSorters.JVM)
+public class _3_HttpClientServerTest {
+
+    @Test
+    public void testsClientServerOverHttp() throws InterruptedException, IOException {
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://yandex.ru"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(__, response.statusCode());
+        assertEquals(__, response.version());
+        assertEquals(__, response.headers().map().size());
+        assertEquals(__, response.body().contains("<html"));
+
+    }
+
+}
---
+++ src/test/java/ru/compscicenter/java_2019/lesson_08/_4_JettyClientServerTest.java
@@ -0,0 +1,87 @@
+package ru.compscicenter.java_2019.lesson_08;
+
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static ru.compscicenter.java_2019.Util.__;
+
+@FixMethodOrder(MethodSorters.JVM)
+public class _4_JettyClientServerTest {
+
+    private Server server;
+
+    static public class SimpleServlet extends HttpServlet {
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("text/html");
+            String mirror = new String(request.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            response.getWriter().print(mirror);
+        }
+
+    }
+
+    @Before
+    public void setup() throws Exception {
+        server = new Server();
+
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+
+        server.setConnectors(new Connector[]{connector});
+
+        ServletHandler servletHandler = new ServletHandler();
+        server.setHandler(servletHandler);
+
+        servletHandler.addServletWithMapping(SimpleServlet.class, "/mirror");
+        server.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+
+    @Test
+    public void testsClientServerOverHttpToJetty() throws InterruptedException, IOException {
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/mirror"))
+                .method("GET", HttpRequest.BodyPublishers.ofString("what's a difference a day made?"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(__, response.statusCode());
+        assertEquals(__, response.body());
+
+    }
+
+}
